4f6ebf707b0b8540a1da7abbbd708e37ab91643d
errachete <raherrer@ucm.es>
2020-03-07 11:12:21 +0100

    Añadido comparador con ode45

---
+++ comp2ode45.m
@@ -0,0 +1,69 @@
+%% Comparador de un método con la función ode45
+
+function comp2ode45(met,  prob)
+% La función comp2ode45 resuelve un problema de valor inicial de la forma
+%  x'=f(t,x) en [t0,Y]
+%  x(t0)=x0
+% con x0 en R^n, mediante el método que se le pasa por parámetro, y 
+% respresenta:
+%  La solución obtenida por el método
+%  La diferencia entre las soluciones obtenidas por el método y la función
+%  ode45
+%  La trayectoria obtenida por el método (sólo si la dimensión es 2 o 3)
+%  El máximmo de la diferencia entre la solución obtenida por el método y
+%  la obtenida por la función ode45
+%
+% ENTRADA:
+%  met: método con el que se pretende resolver el problema
+%  prob: número del problema que se quiere resolver (consultar "datos.m")
+%
+% SALIDA:
+%  No hay parámetros de salida
+
+datos
+[t1,x1] = met(f, intervalo, x0, N);
+[t2,x2] = ode45(f,[intervalo(1):(intervalo(2)-intervalo(1))/N:intervalo(2)],x0);
+
+dim = size(x1,2);
+colors = ['r' 'g' 'b'];
+figure(1)
+for i = 1:dim
+    subplot(dim,1,i)
+    plot(t1,x1(:,i),colors(i))
+    s = sprintf('Coordenada %i de la solución', i);
+    title(s)
+end
+
+pause(2)
+dif = x1-x2;
+figure(2)
+for i = 1:dim
+    subplot(dim,1,i)
+    plot(t1,dif(:,i),colors(i))
+    s = sprintf('Diferencia en la coordenada %i de la solución', i);
+    title(s)
+end
+
+if dim == 2
+    pause(2)
+    figure(3)
+    plot(x1(:,1),x1(:,2))
+    title('Trayectoria de la solución')
+elseif dim == 3
+    pause(2)
+    figure(3)
+    plot3(x1(:,1),x1(:,2),x1(:,3))
+    title('Trayectoria de la solución')
+end
+
+pause(2)
+figure(4)
+ninf = max(max(x1-x2));
+for i = 1:dim
+    plot([0 1],[ninf ninf],colors(i))
+    s = sprintf('El máximo es: %i', ninf);
+    legend(s)
+    title('Máximo de la diferencia entre soluciones')
+end 
+
+end
